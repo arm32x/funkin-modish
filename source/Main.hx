3ec6e0d87504770d2f2011f9a3599eec845428c3
arm32x <arm32.x@gmail.com>
2021-07-08 12:06:23 -0400

    Don’t forcefully disable FPS counter on mobile.

--- source/Main.hx
+++ source/Main.hx
@@ -95,12 +95,9 @@ class Main extends Sprite
 		#end
 		addChild(game);
 		
-		#if !mobile
 		fpsCounter = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsCounter);
 		toggleFPS(FlxG.save.data.fps);
-
-		#end
 	}
 
 	var game:FlxGame;
