23e6bd02927f834c61b00760f300073f18325cf9
arm32x <arm32.x@gmail.com>
2022-05-04 13:41:57 -0400

    Fix extension-webm when linking statically
    The real fix is in my fork of extension-webm; the changes in this commit
    just update some code to work with that fork.

--- source/funkin/PlayState.hx
+++ source/funkin/PlayState.hx
@@ -2847,9 +2847,9 @@ class PlayState extends MusicBeatState
 					FlxG.stage.window.onFocusIn.add(focusIn);
 
 					var ourSource:String = "assets/videos/daWeirdVid/dontDelete.webm";
-					WebmPlayer.SKIP_STEP_LIMIT = 90;
 					var str1:String = "WEBM SHIT"; 
 					webmHandler = new WebmHandler();
+					webmHandler.webm.SKIP_STEP_LIMIT = 90;
 					webmHandler.source(ourSource);
 					webmHandler.makePlayer();
 					webmHandler.webm.name = str1;
